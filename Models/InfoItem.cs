0d160923afe9c31f64c28526cff5c17b8e8ee202
Yehuda <109089553+YehudaKahn@users.noreply.github.com>
2022-12-18 01:31:11 +0200

    new pages & assets

---
+++ Models/InfoItem.cs
@@ -0,0 +1,11 @@
+
+
+namespace  ChabadNapa.Models;
+
+public struct InfoItem
+{
+    public string ImgUrl { get; set; }
+    public string Link { get; set; }
+    public string Title { get; set; }
+    public string Text { get; set; }
+}
